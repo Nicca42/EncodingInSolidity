3c60576aa36ad8d027ec59ef4b5fd20537521ebd
Veronica <veronica.21.coutts@gmail.com>
2018-11-21 11:47:01 +0200

    Added Comments

--- contracts/encodedFunction.sol
+++ contracts/encodedFunction.sol
@@ -4,6 +4,8 @@ contract encodedfunctions {
     bool public run = false;
     bool public lopRun = false;
     bool public paeRun = false;
+    bool public thRun = false;
+    bool public teRun = false;
     address public owner;
     mapping(address => uint256) public balances;
 
@@ -11,19 +13,56 @@ contract encodedfunctions {
         owner = msg.sender;
     }
     
+    /**
+      * @dev This function takes in a number, performs 
+      * an operation on it, and then sets its check bool
+      * to true. This check bool serves as a check so in testing
+      * we can see the function definatly ran.
+      * @param _input : the number to manipulate
+      */
     function toEncode(uint8 _input) public {
         _input + 2;
         run = true;
     }
 
+    /**
+      * @dev This function calls the above function, after encoding it.
+      * The function can be encoded in solidity or in JS. 
+      */
     function decode() public {
+        /** calls this (the contract)
+          * then uses the call function to call the encoded function. 
+          * The function is passed the uint 87, (its paramiter)
+          */
         this.call(bytes32(keccak256("toEncode(uint8)")), 87);
+        /** note, the .call function will not revert if it fails, 
+          * as it returns a bool.
+          * The call should be inside a revert, but as there is the 
+          * a run bool check, we will know if the function failed 
+          * in execution.
+          */ 
     }
     
+    /**
+      * @dev This function takes in an encoded function call.
+      * the function is encoded in JS. It then calls this function 
+      * through the call function. 
+      * @param _encoded : The encoded function call
+      */
     function decodeAttTwo(bytes32 _encoded) public {
+        //require so that the .call has to pass
         require(this.call(_encoded));
     }
 
+    /**
+      * @dev This function calls a function like decodeAttTwo,
+      * except it gets passed the encoded functon signature and
+      * encoded paramiters seporatly. 
+      * This was to test that the function signature and prameters 
+      * can be passed separatly. 
+      * @param _function : bytes32 : the encoded function signature
+      * @param _callData : bytes32 : the encoded paramiters. 
+      */
     function seperationOfCallDataAndFunction(
         bytes32 _function, 
         bytes32 _callData
@@ -33,52 +72,58 @@ contract encodedfunctions {
         require(this.call(_function, _callData));
     }
 
+    /**
+      * @dev This function checks that encoding of multiple 
+      * params can still be called through the .call function.
+      * @param _value : uint256 : a uint 
+      * @param _sender : address : an address 
+      */
     function lotsOfParam(uint256 _value, address _sender) public {
         balances[_sender] -= _value;
+        //the functions execution check
         lopRun = true;
     }
 
-    function paramsAndEncoded(
-        uint _value, 
-        address _sender, 
-        bytes _encodedFunction
-    ) 
-        public 
-    {
-        /**we want to be able to see which function is being called
-            and that the call is autherized: 
-            Ideas: 
-                including the msg.sender in the encoded function
-            check:
-                need to check that the authorization can be done
-                implicity 
-                    make a onlyAddress function and then 
-                    try call it from the correct and incorect 
-                    address 
-        */
-    }
-
+    /**
+      * @dev This access modifier is used to check the access 
+      * on calling functions through an encoded function call.
+      * Owner is set in the constructor.
+      */
     modifier onlyAnAddress() {
         require(msg.sender == owner);
         _;
     }
 
-    function theThing() public onlyAnAddress() {
-        //this must be the function that dose the 
-        //encoded thing. 
-        //check that it cannot be done by wrong address
-        //also that it can be doen by correct address.
+    /**
+      * @dev this uses .call, but it is access modified to check 
+      * how the access modifiers will be affected by modifiers. 
+      * @param _encoded : the encoded function call.
+      */
+    function theThing(bytes _encoded) public onlyAnAddress() {
+        require(this.call(_encoded));
+        thRun = true;
     }
 
-
-   /**
-    fallback(uint value, address sender, bytes encodedFunction)
-    this.call(extractedFunction, extractedCalldata) (edited)
-    So instead of passing the calldata variable by variable, 
-    we need to pass the raw encoded data and hope the function runs
-
-
-    balances[_sender] -= _value;
-        paeRun = true;
-    */
+    event theEmitterEvent(string _str, uint _value, bool _tf, string _str2);
+    
+    /**
+      * @dev this function takes in multiple arrays of unfixed length. 
+      * This is to check the bytes32 vs bytes. It also emits its 
+      * params as to check correct decoding. 
+      * @param _str : an array of unfixed size 
+      * @param _value : a random number
+      * @param _tf : a bool
+      * @param _str2 : another array 
+      */
+    function theEmmiter(
+        string _str, 
+        uint _value, 
+        bool _tf, 
+        string _str2
+    )
+        public
+    {
+        emit theEmitterEvent(_str, _value, _tf, _str2);
+        teRun = true;
+    }
 }
--- test/encodedfunctions.test.js
+++ test/encodedfunctions.test.js
@@ -117,5 +117,120 @@ contract('EncodedFunctions', (accounts) => {
         assert.equal(running, true, "The function check is true");
     });
 
-    
+    it("Checking user address passing", async () => {
+        encodedFunctions = await EncodedFunctions.new({from: owner});
+        let runningBefore = await encodedFunctions.thRun();
+        assert.equal(runningBefore, false, "The function check is set to false");
+        let abiSignatureOfFunctionToCall = web3Abi.encodeFunctionCall(
+            {
+                "constant": false,
+                "inputs": [
+                  {
+                    "name": "_value",
+                    "type": "uint256"
+                  },
+                  {
+                    "name": "_sender",
+                    "type": "address"
+                  }
+                ],
+                "name": "lotsOfParam",
+                "outputs": [],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "function"
+            }, ['116', '0xa8A7f96b9c4a1208cC4fEcD602Cdde45A23d73a7']
+        )
+        assert.equal(
+            abiSignatureOfFunctionToCall,
+            "0x7b2e1b8d0000000000000000000000000000000000000000000000000000000000000074000000000000000000000000a8a7f96b9c4a1208cc4fecd602cdde45a23d73a7", 
+            "The function to be called is encoded correctly"
+        );
+        let abiSignature = web3Abi.encodeFunctionCall({
+                "constant": false,
+                "inputs": [
+                {
+                    "name": "_encoded",
+                    "type": "bytes"
+                }
+                ],
+                "name": "theThing",
+                "outputs": [],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "function"
+            }, [abiSignatureOfFunctionToCall]
+        )
+        assert.equal(
+            "0x23841c8d000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000447b2e1b8d0000000000000000000000000000000000000000000000000000000000000074000000000000000000000000a8a7f96b9c4a1208cc4fecd602cdde45a23d73a700000000000000000000000000000000000000000000000000000000", 
+            abiSignature,
+            "The function is encoded correctly"
+        );
+        encodedFunctions.theThing(abiSignatureOfFunctionToCall, {from: owner})
+        let running = await encodedFunctions.thRun();
+        assert.equal(running, true, "The function has run");
+    });
+
+    it("Checking the emitter", async () => {
+        encodedFunctions = await EncodedFunctions.new({from: owner});
+        let abiSignatureOfFunctionToCall = web3Abi.encodeFunctionCall({
+                "constant": false,
+                "inputs": [
+                {
+                    "name": "_str",
+                    "type": "string"
+                },
+                {
+                    "name": "_value",
+                    "type": "uint256"
+                },
+                {
+                    "name": "_tf",
+                    "type": "bool"
+                },
+                {
+                    "name": "_str2",
+                    "type": "string"
+                }
+                ],
+                "name": "theEmmiter",
+                "outputs": [],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "function"
+            }, ["this is a string. It is actually a really long string, one could say, absurdly long.", 777, true, "this is a string. It is actually a really long string, one could say, absurdly long."]
+        );
+        assert.equal(
+            abiSignatureOfFunctionToCall,
+            "0x6de1dda10000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000030900000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000005474686973206973206120737472696e672e2049742069732061637475616c6c792061207265616c6c79206c6f6e6720737472696e672c206f6e6520636f756c64207361792c206162737572646c79206c6f6e672e000000000000000000000000000000000000000000000000000000000000000000000000000000000000005474686973206973206120737472696e672e2049742069732061637475616c6c792061207265616c6c79206c6f6e6720737472696e672c206f6e6520636f756c64207361792c206162737572646c79206c6f6e672e000000000000000000000000",
+            "The encoded function is correct"
+        );
+        let abiSignature = web3Abi.encodeFunctionCall({
+                "constant": false,
+                "inputs": [
+                {
+                    "name": "_encoded",
+                    "type": "bytes"
+                }
+                ],
+                "name": "theThing",
+                "outputs": [],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "function"
+            }, [abiSignatureOfFunctionToCall]
+        );
+        assert.equal(
+            abiSignature,
+            "0x23841c8d000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000001846de1dda10000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000030900000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000005474686973206973206120737472696e672e2049742069732061637475616c6c792061207265616c6c79206c6f6e6720737472696e672c206f6e6520636f756c64207361792c206162737572646c79206c6f6e672e000000000000000000000000000000000000000000000000000000000000000000000000000000000000005474686973206973206120737472696e672e2049742069732061637475616c6c792061207265616c6c79206c6f6e6720737472696e672c206f6e6520636f756c64207361792c206162737572646c79206c6f6e672e00000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "The encoded function call is correct"
+        );
+        encodedFunctions.theThing(abiSignatureOfFunctionToCall, {from: owner});
+        let runningTheTing = await encodedFunctions.thRun();
+        assert.equal(runningTheTing, true, "The function executed");
+        let runningBefore = await encodedFunctions.teRun();
+        assert.equal(runningBefore, true, "The function executed");
+        // assert.equal(true, false, "Getting events");
+        //^^ un-comment to see the event emitted with correct data 
+    });
 });
